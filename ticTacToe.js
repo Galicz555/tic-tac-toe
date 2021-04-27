ff156e9f1a6c0a8c7a403e3f2c8c6c1571a9bbf6
Galicz Mihály <galicz555@gmail.com>
2021-04-27 19:36:29 +0200

    Added mark styling
    1) little cleanup

--- ticTacToe.js
+++ ticTacToe.js
@@ -2,14 +2,19 @@
 
 let ticTacToe = {
 	init: function() {
+		ticTacToe.players = [];
+
 		ticTacToe.board = document.getElementById('board')
 		ticTacToe.columnSetterField = document.getElementById('columnSetterField'),
 		ticTacToe.columnSetterFieldValue = document.getElementById('columnSetterField').value;
+
+		ticTacToe.squares = document.getElementsByClassName('board__square');
+
 		this.setHtmlBoardSize();
-		this.boardChangeListener();
+		this.addBoardChangeListener();
 	},
 
-	boardChangeListener: function() {
+	addBoardChangeListener: function() {
 		this.columnSetterField.addEventListener('change', (e) => {
 			document.documentElement.style.setProperty('--columnRow', e.target.value);
 			this.clearHtmlBoard();
@@ -18,16 +23,30 @@ let ticTacToe = {
 		})
 	},
 
+	addSquareClickListener: function() {
+		this.squares.forEach(element => {
+			element.addEventListener('click', handleClick, { once: true })
+		});
+	},
+
+	handleClick: function() {
+	},
+
 	setHtmlBoardSize: function() {
 		let squareArray = [];
 		for (let i = 0; i < this.columnSetterFieldValue * this.columnSetterFieldValue; i++) {
 			squareArray[i] = document.createElement('div');
+			if (i === 0) {
+				squareArray[i].className = 'board__square x';
+				squareArray[i].textContent = 'X'
+			} else {
 			squareArray[i].className = 'board__square';
-			squareArray[i].textContent = i;
+			}
 			
 			this.board.appendChild(squareArray[i]);
 		}
 	},
+
 
 	clearHtmlBoard: function() {
 		while (this.board.firstChild) {
