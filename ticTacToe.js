63001cb543a628da5fea717cb2cc5b1abe376349
Galicz Mihály <galicz555@gmail.com>
2021-04-27 20:34:29 +0200

    Added player options generator

--- ticTacToe.js
+++ ticTacToe.js
@@ -4,12 +4,18 @@ let ticTacToe = {
 	init: function() {
 		ticTacToe.players = [];
 
-		ticTacToe.board = document.getElementById('board')
-		ticTacToe.columnSetterField = document.getElementById('columnSetterField'),
+		ticTacToe.playerOptions = document.getElementById('players__options');
+		ticTacToe.playersNumber = document.getElementById('playersNumber');
+		ticTacToe.playersNumberValue = document.getElementById('playersNumber').value;
+
+		ticTacToe.board = document.getElementById('board');
+		ticTacToe.columnSetterField = document.getElementById('columnSetterField');
 		ticTacToe.columnSetterFieldValue = document.getElementById('columnSetterField').value;
 
 		ticTacToe.squares = document.getElementsByClassName('board__square');
 
+		this.setNumberOfPlayerOptions();
+		this.addPlayerNumberChangeListener();
 		this.setHtmlBoardSize();
 		this.addBoardChangeListener();
 	},
@@ -18,9 +24,50 @@ let ticTacToe = {
 		this.columnSetterField.addEventListener('change', (e) => {
 			document.documentElement.style.setProperty('--columnRow', e.target.value);
 			this.clearHtmlBoard();
-			this.columnSetterFieldValue = document.getElementById('columnSetterField').value;
+			this.columnSetterFieldValue = e.target.value;
 			this.setHtmlBoardSize();
 		})
+	},
+
+	addPlayerNumberChangeListener: function() {
+		this.playersNumber.addEventListener('change', (e) => {
+			this.clearPlayerOptions();
+			this.playersNumberValue = e.target.value;
+			this.setNumberOfPlayerOptions();
+		})
+	},
+
+	setNumberOfPlayerOptions: function() {
+		let playerNameArray = [];
+		let playerMarkArray = [];
+		let playerNameInputArray = [];
+		let playerMarkInputArray = [];
+		for (let i = 0; i < this.playersNumberValue; i++) {
+			playerNameArray[i] = document.createElement('div');
+			playerNameArray[i].className = 'player__field--name';
+			playerNameArray[i].textContent = `Player${i+1}: `;
+
+			playerNameInputArray[i] = document.createElement('input');
+			playerNameInputArray[i].className = 'player__field--nameInput';
+
+			playerMarkArray[i] = document.createElement('div');
+			playerMarkArray[i].className = 'player__field--mark';
+			playerMarkArray[i].textContent = 'Sign to use: ';
+
+			playerMarkInputArray[i] = document.createElement('input');
+			playerMarkInputArray[i].className = 'player__field--markInput';
+			
+			this.playerOptions.appendChild(playerNameArray[i]);
+			this.playerOptions.appendChild(playerNameInputArray[i]);
+			this.playerOptions.appendChild(playerMarkArray[i]);
+			this.playerOptions.appendChild(playerMarkInputArray[i]);
+		}
+	},
+
+	clearPlayerOptions: function() {
+		while (this.playerOptions.firstChild) {
+			this.playerOptions.removeChild(this.playerOptions.firstChild);
+		}
 	},
 
 	addSquareClickListener: function() {
@@ -36,17 +83,11 @@ let ticTacToe = {
 		let squareArray = [];
 		for (let i = 0; i < this.columnSetterFieldValue * this.columnSetterFieldValue; i++) {
 			squareArray[i] = document.createElement('div');
-			if (i === 0) {
-				squareArray[i].className = 'board__square x';
-				squareArray[i].textContent = 'X'
-			} else {
 			squareArray[i].className = 'board__square';
-			}
 			
 			this.board.appendChild(squareArray[i]);
 		}
 	},
-
 
 	clearHtmlBoard: function() {
 		while (this.board.firstChild) {
